04e5cd4a413a47d725c99da7e632a9a023420f44
棟方俊貴 <munakata.toshiki.likeit@gmail.com>
2020-02-19 18:54:16 +0900

    no message

---
+++ SQL/favoul_table.sql
@@ -0,0 +1,65 @@
+create table delivery_method(
+id int PRIMARY KEY,
+name varchar(256) UNIQUE not null,
+price int not null
+);
+
+create table item(
+id int PRIMARY KEY,
+name varchar(256) NOT NULL,
+detail text NOT NULL,
+price int NOT NULL,
+file_name varchar(256) NOT NULL,
+item_category_id int NOT NULL,
+create_date date NOT NULL
+);
+
+
+create table user(
+id int PRIMARY KEY,
+name varchar(256) NOT NULL,
+address varchar(256) NOT NULL,
+password varchar(256) NOT NULL,
+birth_date date NOT NULL,
+create_date date NOT NULL,
+)
+
+create table item_category(
+id int PRIMARY KEY,
+category varchar(100) NOT NULL
+)
+
+create table trivia(
+id int PRIMARY KEY,
+name varchar(256) NOT NULL,
+detail text NOT NULL,
+item_category_id int NOT NULL
+)
+
+create table buy(
+id int PRIMARY KEY,
+user_id int NOT NULL,
+total_price int NOT NULL,
+delivery_method_id int NOT NULL,
+create_date datetime NOT NULL
+)
+
+
+create table buy_detail(
+id int PRIMARY KEY,
+buy_id int NOT NULL,
+item_id NOT NULL
+)
+
+create table food(
+id int PRIMARY KEY,
+name varchar(256) NOT NULL,
+detail text NOT NULL,
+file_name varchar(256) NOT NULL
+)
+
+create table food_best_much(
+id int PRIMARY KEY,
+food_id int NOT NULL,
+item_id int NOT NULL
+)
